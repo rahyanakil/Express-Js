7c67b978e6c30c3bfbd61cdfaa6389268c9ae582
rahyanakil <rahyanakil89@gmail.com>
2025-02-08 16:34:31 +0600

    file cleared

--- routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Sample Route
-router.get("/hello", (req, res) => {
-  res.json({ message: "Hello from Express API" });
-});
-
-module.exports = router;
